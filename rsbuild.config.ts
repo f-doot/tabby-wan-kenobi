db5018bd20228790640cc85762fbcfee901356e1
Francis <francis.screene@protonmail.com>
2026-05-29 20:43:12 +0100

    Fix path

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,4 +4,8 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  output: {
+    // Crucial: Must start and end with a slash, matching your GitHub repo name
+    assetPrefix: '/tabby-wan-kenobi/',
+  },
 });
