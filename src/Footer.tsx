df19cbef4ebcd6f0806fbaee22fe7e2a14a3bdc3
Francis <francis.screene@protonmail.com>
2026-05-30 20:19:13 +0100

    Add missing file

---
+++ src/Footer.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      Made with Love in the United Kingdom. Always work in progress.
+    </div>
+  );
+}
